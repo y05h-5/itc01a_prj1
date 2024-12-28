2df64048e07809a8a59f5e4009c5c93dd1594d38
y05h-5 <asuka@asuka.local>
2024-12-29 00:44:59 +0900

    user input

--- Project1.java
+++ Project1.java
@@ -4,12 +4,14 @@ import java.util.Scanner;
 
 import javax.swing.*;
 import java.nio.file.*;
-// import java.io.*;
 
 public class Project1 {
     public static void main(String[] args) {
         JFrame jf = new JFrame("Project 1");
-        DrawingPanel dp = new DrawingPanel("vert/disk.vert");
+        DrawingPanel dp = (args.length < 1)?
+            new DrawingPanel("vert/disk.vert") :
+            new DrawingPanel(args[0]);
+
         jf.add(dp);
         jf.setSize(800,600);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -39,21 +41,22 @@ class DrawingPanel extends JPanel {
         int cmp = input.nextInt();
         for (int c = 0; c < cmp; ++c) {
             int vrt = input.nextInt();
-
-            Double x0 = input.nextDouble() * 100 + (width/2);
-            Double y0 = input.nextDouble() * 100 + (height/2);
+            System.out.println(vrt);
+            Double x0 = -1 * (input.nextDouble()) + (width/2);
+            Double y0 = -1 * (input.nextDouble()) + (height/2);
             Double prevX = x0;
             Double prevY = y0;
 
             for (int v = 1; v < vrt; ++v) {
-                Double x = input.nextDouble() * 100 + (width/2);
-                Double y = input.nextDouble() * 100 + (height/2);
+                Double x = -1 * (input.nextDouble()) + (width/2);
+                Double y = -1 * (input.nextDouble()) + (height/2);
                 g2d.draw(new Line2D.Double(prevX, prevY, x, y));
                 prevX = x;
                 prevY = y;
             }
             g2d.draw(new Line2D.Double(prevX, prevY, x0, y0));
         }
+
         input.close();
     }
 }
