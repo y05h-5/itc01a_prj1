5c9df2fe1535a4bc9526b345d61f98db738aeb32
y05h-5 <asuka@asuka.local>
2024-12-28 22:16:29 +0900

    vert scanner, basic display done

--- Project1.java
+++ Project1.java
@@ -1,13 +1,15 @@
 import java.awt.*;
 import java.awt.geom.*;
+import java.util.Scanner;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
+import java.nio.file.*;
+// import java.io.*;
 
 public class Project1 {
     public static void main(String[] args) {
         JFrame jf = new JFrame("Project 1");
-        DrawingPanel dp = new DrawingPanel();
+        DrawingPanel dp = new DrawingPanel("vert/disk.vert");
         jf.add(dp);
         jf.setSize(800,600);
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,11 +18,42 @@ public class Project1 {
 }
 
 class DrawingPanel extends JPanel {
+    private Scanner input;
+
+    public DrawingPanel (String fname) {
+        Path fp = Paths.get(fname);
+        try {
+            input = new Scanner(fp);
+        } catch (Exception e) {
+            System.err.println("Scanner creation failed");
+        }
+    }
+
     @Override
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
-        Point2D.Double p1 = new Point2D.Double(100,100);
-        Point2D.Double p2 = new Point2D.Double(200,300);
-        g2d.draw(new Line2D.Double(p1,p2));
+
+        int height = getHeight();
+        int width = getWidth();
+
+        int cmp = input.nextInt();
+        for (int c = 0; c < cmp; ++c) {
+            int vrt = input.nextInt();
+
+            Double x0 = input.nextDouble() * 100 + (width/2);
+            Double y0 = input.nextDouble() * 100 + (height/2);
+            Double prevX = x0;
+            Double prevY = y0;
+
+            for (int v = 1; v < vrt; ++v) {
+                Double x = input.nextDouble() * 100 + (width/2);
+                Double y = input.nextDouble() * 100 + (height/2);
+                g2d.draw(new Line2D.Double(prevX, prevY, x, y));
+                prevX = x;
+                prevY = y;
+            }
+            g2d.draw(new Line2D.Double(prevX, prevY, x0, y0));
+        }
+        input.close();
     }
 }
