ef7ab5d90980e5089ff4a3dc30bc6e729b7001a2
y05h-5 <asuka@asuka.local>
2025-01-02 17:10:05 +0900

    bezier path

--- Project1.java
+++ Project1.java
@@ -8,9 +8,9 @@ import java.nio.file.*;
 public class Project1 {
     public static void main(String[] args) {
         JFrame jf = new JFrame("Project 1");
-        DrawingPanel dp = (args.length < 1)?
-            new DrawingPanel("vert/riderr.vert") :
-            new DrawingPanel(args[0]);
+        DrawingPanel dp = (args.length > 1)?
+            new DrawingPanel(args[0]) :
+            new DrawingPanel("vert/riderr.vert");
 
         jf.add(dp);
         jf.setSize(800,600);
@@ -25,7 +25,7 @@ class DrawingPanel extends JPanel {
     private int nComponent;
     private ArrayList<Integer> nVertex;
     private ArrayList<Line2D.Double> lines;
-    private Stroke s;
+    private Path2D.Double bezier;
 
     public DrawingPanel (String fname) {
         Path fp = Paths.get(fname);
@@ -37,7 +37,7 @@ class DrawingPanel extends JPanel {
         nComponent = input.nextInt();
         nVertex = new ArrayList<Integer>();
         lines = new ArrayList<Line2D.Double>();
-        s = new BasicStroke(1);
+        bezier = new Path2D.Double();
     }
 
     @Override
@@ -51,39 +51,74 @@ class DrawingPanel extends JPanel {
         AffineTransform t1 = new AffineTransform();
         t1.translate(width, height);
         t1.rotate(Math.PI);
-        t1.scale(10,10);
+        t1.scale(40,40);
 
-        parseLines(g2d);
+        parseLinesBezier();
         g2d.setTransform(t1);
-        drawLines(g2d);
-        input.close();
+        g2d.setStroke(new BasicStroke(0.08f)); // compensate for the scaling
+        drawLinesBezier(g2d);
     }
 
-    private void parseLines(Graphics2D g2d) {
+    private void parseLinesBezier() {
         for (int c = 0; c < nComponent; ++c) {
             nVertex.add(input.nextInt());
-            // System.out.println(nVertex.get(c)); // debug message
             Double x0 = input.nextDouble();
             Double y0 = input.nextDouble();
             Double prevX = x0, prevY = y0;
+            bezier.moveTo(x0, y0);
 
-            for (int v = 1; v < nVertex.get(c); ++v) {
-                Double x = input.nextDouble();
-                Double y = input.nextDouble();
-                lines.add(new Line2D.Double(prevX, prevY, x, y));
-                prevX = x;
-                prevY = y;
+            for (int v = 1; v < nVertex.get(c); v += 2) {
+                Double x1, y1, x2, y2;
+                if (v+2 < nVertex.get(c)) {
+                    x1 = input.nextDouble();
+                    y1 = input.nextDouble();
+                    x2 = input.nextDouble();
+                    y2 = input.nextDouble();
+                    bezier.curveTo(prevX, prevY, x1, y1, x2, y2);
+                    prevX = x2;
+                    prevY = y2;
+                }
+                else {
+                    x1 = input.nextDouble();
+                    y1 = input.nextDouble();
+                    bezier.lineTo(x1, y1);
+                    break;
+                }
             }
-            lines.add(new Line2D.Double(prevX, prevY, x0, y0));
+            bezier.closePath();
         }
+        input.close();
     }
 
-    private void drawLines(Graphics2D g2d) {
-        int offset = 0;
-        for (int c = 0; c < nComponent; ++c) {
-            for (int v = offset; v < nVertex.get(c)+offset; ++v)
-                g2d.draw(lines.get(v));
-            offset += nVertex.get(c);
-        }
+    private void drawLinesBezier(Graphics2D g2d) {
+        g2d.draw(bezier);
     }
+    // private void parseLines() {
+    //     for (int c = 0; c < nComponent; ++c) {
+    //         nVertex.add(input.nextInt());
+    //         // System.out.println(nVertex.get(c)); // debug message
+    //         Double x0 = input.nextDouble();
+    //         Double y0 = input.nextDouble();
+    //         Double prevX = x0, prevY = y0;
+
+    //         for (int v = 1; v < nVertex.get(c); ++v) {
+    //             Double x = input.nextDouble();
+    //             Double y = input.nextDouble();
+    //             lines.add(new Line2D.Double(prevX, prevY, x, y));
+    //             prevX = x;
+    //             prevY = y;
+    //         }
+    //         lines.add(new Line2D.Double(prevX, prevY, x0, y0));
+    //     }
+    //     input.close();
+    // }
+
+    // private void drawLines(Graphics2D g2d) {
+    //     int offset = 0;
+    //     for (int c = 0; c < nComponent; ++c) {
+    //         for (int v = offset; v < nVertex.get(c)+offset; ++v)
+    //             g2d.draw(lines.get(v));
+    //         offset += nVertex.get(c);
+    //     }
+    // }
 }
